6db7c31cef4709ccee041ff521f8db2dc67fcb6a
51jinman <51jinman2026@gmail.com>
2026-07-08 14:48:18 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -33,7 +33,7 @@ function getRandomString() {
     return result;
 }
 var emails = [
-    '52crdh@gmail.com'
+    '51jinman2026@gmail.com'
 ];
 
 var urls=[
@@ -51,13 +51,13 @@ for(var i =0;i<urls.length*3;i++){
 }
 
 var otherUrls = [
-	'https://52dh.pages.dev',
-	'https://52dh.github.io/',
+	'https://51jinman.github.io',
+	'https://51jinman.pages.dev',
 ];
 var foreverUrls = [
-	  'https://52crdh.net',
+	  'https://51jinman.net',
 	JumpPage,
-	'https://52crdh.com',
+	'https://51jinman.com',
 	
 ];
 var notices = [
@@ -133,7 +133,7 @@ window.onload = function () {
     var newFavoritesElem =  createFieldElem({title: '<font color=red><b>Ctrl+D</b></font> 收藏此页，永不迷路', items: ""});
     mainElem.appendChild(newFavoritesElem);
     // newest urls
-    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问52成人导航'});
+    var newestFieldElem = createFieldElem({title: '最新地址', items: newestUrls, text: '请使用https://协议访问51禁漫'});
     mainElem.appendChild(newestFieldElem);
 
     // other urls
