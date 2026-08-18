dc28129052b625e3c843190697383f939521e2b3
51jinman <51jinman2026@gmail.com>
2026-08-18 10:05:53 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -39,7 +39,6 @@ var emails = [
 var urls=[
 	'hygjovkp.com', 
 	'hygjovkp.com', 
-    'hygjovkp.com',
 ];                                                                                                                  
 
 var JumpPage="https://51jinman.com";
