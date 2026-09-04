e3341ad7f7a6b68da3f9db180915f0d5a5c10bc2
51jinman <51jinman2026@gmail.com>
2026-09-04 14:14:52 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'elmzwarjy.cc', 
+	'fqosrslqp.cc', 
 ];                                                                                                                  
 
 var JumpPage="https://51jinman.com";
