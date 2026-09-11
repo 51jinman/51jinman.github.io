e6076081ddcbfa09ade9baa9b7de9f180c3b8f48
51jinman <51jinman2026@gmail.com>
2026-09-11 14:54:24 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'lmvmhjtnu.cc', 
+	'aqpvchlm.cc', 
 ];                                                                                                                  
 
 var JumpPage="https://51jinman.com";
