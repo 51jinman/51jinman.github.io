b170c8282d3539ed93ba8229bb4a3b264b1e4d69
51jinman <51jinman2026@gmail.com>
2026-09-18 09:11:29 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'snibazfi.cc', 
+	'alidtzqw.cc', 
 ];                                                                                                                  
 
 var JumpPage="https://51jinman.com";
